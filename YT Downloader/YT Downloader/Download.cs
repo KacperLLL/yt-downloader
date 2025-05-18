b8325e1ad8459bb9f1a6c1b7428e7340c49d755e
KacperLLL <kacperzak1980@gmail.com>
2025-05-18 13:01:57 +0200

    initial commit

---
+++ YT Downloader/YT Downloader/Download.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Converter;
+using YoutubeExplode.Videos.Streams;
+
+namespace YT_Downloader
+{
+    internal class Download
+    {
+        private readonly YoutubeClient _youtube;
+        private string _url;
+        private string _save_path;
+        private bool _is_busy;
+
+        public Download(string URL, string SAVE_PATH) { 
+                _url= URL;
+                _youtube = new YoutubeClient();
+                _save_path= SAVE_PATH;
+        }
+
+        public async Task StartDownload()
+        {
+            Console.WriteLine("Rozpoczenczie pobierania: ");
+            try
+            {
+                _is_busy = true;
+                await youtube.Videos.DownloadAsync(_url, _save_path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Zakonczono pobieranie");
+                _is_busy= false;
+            }
+        }
+
+
+        public YoutubeClient youtube { get { return _youtube; } }
+        public string url { get { return _url;} set { _url = value; } }
+        public string save_path { get { return _save_path;} set { _save_path = value; } }
+        public bool is_busy { get { return _is_busy; } }
+    }
+}
---
+++ YT Downloader/YT Downloader/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static YT_Downloader.Download;
+
+namespace YT_Downloader
+{
+    internal class Start
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("Podaj link do filmu z YouTube:");
+            var videoUrl = Console.ReadLine();
+            var path = "video.mp4";
+
+            if(videoUrl is not null)
+            {
+                Download download = new Download(videoUrl, path);
+                
+                await download.StartDownload();
+            }
+            else
+            {
+                Console.WriteLine("BLAD");
+            }
+        }
+    }
+}
