ce0c81bafbbf137ccf4c75091b4a6f5763611928
KacperLLL <kacperzak1980@gmail.com>
2025-06-10 14:27:20 +0200

    Dlasze prace nad responsywnością, oraz porządki w katalogu

--- YT_DOWNLOADER/YT_DOWNLOADER/GUI/hide.js
+++ YT_DOWNLOADER/YT_DOWNLOADER/GUI/hide.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("load", function() {
     hideElements();
 });
 
@@ -9,7 +9,7 @@ window.addEventListener("resize", function() {
 function hideElements() 
     {
         const home = document.querySelectorAll(".link_a");
-    home.forEach(element => {
+        home.forEach(element => {
         if(element.dataset.button === "home")
         {
             if(window.innerWidth < 800)
@@ -71,6 +71,100 @@ function hideElements()
         }else
         {
             mainPage.style.marginLeft = "150px";
+        }
+    }
+
+    const video = document.querySelectorAll("iframe");
+    const videoContainer = document.querySelectorAll(".thumbnail");
+    const title = document.querySelectorAll("h2.title");
+    const author = document.querySelectorAll(".lower-data h4");
+    const download_tab = document.querySelectorAll(".download-tab");
+    const buttonDownload = document.querySelectorAll(".download-button div");
+    {
+        if(video.length > 0)
+        {
+            if(window.innerWidth < 800)
+            {
+                videoContainer.forEach(element => {
+                element.style.width = "150px";
+                element.style.height = "85px";
+                });
+            }
+            else
+            {
+                videoContainer.forEach(element => {
+                    element.style.width = "300px";
+                    element.style.height = "170px";
+                });
+            }
+
+            if(window.innerWidth < 800)
+            {
+                video.forEach(element => {
+                element.style.width = "150px";
+                element.style.height = "85px";
+                });
+            }
+            else
+            {
+                video.forEach(element => {
+                    element.style.width = "300px";
+                    element.style.height = "170px";
+                });
+            }
+
+            if(window.innerWidth < 800)
+            {
+                title.forEach(element => {
+                element.style.fontSize = "12px";
+                });
+            }
+            else
+            {
+                title.forEach(element => {
+                    element.style.fontSize = "16px";
+                });
+            }
+
+            if(window.innerWidth < 800)
+            {
+                author.forEach(element => {
+                element.style.fontSize = "10px";
+                });
+            }
+            else
+            {
+                author.forEach(element => {
+                    element.style.fontSize = "12px";
+                });
+            }
+
+            if(window.innerWidth < 800)
+            {
+                download_tab.forEach(element => {
+                element.style.height = "85px";
+                });
+            }
+            else
+            {
+                download_tab.forEach(element => {
+                    element.style.height = "170px";
+                });
+            }
+
+            if(window.innerWidth < 800)
+            {
+                buttonDownload.forEach(element => {
+                element.style.fontSize = "8px";
+                element.style.lineHeight = "4";
+                });
+            }
+            else
+            {
+                buttonDownload.forEach(element => {
+                    element.style.fontSize = "14px";
+                });
+            }
         }
     }
 }
